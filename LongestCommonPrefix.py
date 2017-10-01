8ba02bd593845a53f0d1785917140d352abd6d74
Alan Hu <pianolife22@gmail.com>
2017-10-01 00:50:08 -0700

    Fixed up a bug in this problem.

--- LongestCommonPrefix.py
+++ LongestCommonPrefix.py
@@ -9,21 +9,18 @@ class Solution(object):
         if(len(strs) == 0):
             return ''
         while(True):
-            isMatch = True
             if i >= len(strs[0]):
                 return longestPrefix
             comp = strs[0][i]
             for word in strs:
-                if i >= len(word):
+                if i >= len(word) or comp != word[i]:
                     return longestPrefix
-                if comp != word[i]:
-                    isMatch = False
-                    break
-            longestPrefix = longestPrefix + comp if isMatch else longestPrefix
+                    
+            longestPrefix = longestPrefix + comp
             i += 1
             
         return longestPrefix
 
 
 s = Solution()
-print(s.longestCommonPrefix(['bvdc']))
+print(s.longestCommonPrefix(["aca","cba"]))
