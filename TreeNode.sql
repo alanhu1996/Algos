0fb00c4f52b6ed51680ac1a0c4cc1c08bddb6d85
Alan Hu <pianolife22@gmail.com>
2018-02-09 17:49:22 -0500

    Added solution for tree node db problem.

---
+++ TreeNode.sql
@@ -0,0 +1,29 @@
+-- Given a table tree, id is identifier of the tree node and p_id is its parent node's id.
+
+-- +----+------+
+-- | id | p_id |
+-- +----+------+
+-- | 1  | null |
+-- | 2  | 1    |
+-- | 3  | 1    |
+-- | 4  | 2    |
+-- | 5  | 2    |
+-- +----+------+
+-- Each node in the tree can be one of three types:
+-- Leaf: if the node is a leaf node.
+-- Root: if the node is the root of the tree.
+-- Inner: If the node is neither a leaf node nor a root node.
+
+# Write your MySQL query statement below
+SELECT
+    id AS `Id`,
+    CASE
+        WHEN tree.id = (SELECT atree.id FROM tree atree WHERE atree.p_id IS NULL)
+          THEN 'Root'
+        WHEN tree.id IN (SELECT atree.p_id FROM tree atree)
+          THEN 'Inner'
+        ELSE 'Leaf'
+    END AS Type
+FROM
+    tree
+ORDER BY `Id`;
